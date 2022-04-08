9739a590257ff66517619ade48add46fbcff3af6
Eric <enregistrements@gmail.com>
2022-04-08 15:21:39 +0800

    uppdate todo

--- start.py
+++ start.py
@@ -16,10 +16,14 @@ print(r.url)
 jsess = (parse.urlparse(r.url).params).split("=")[1]
 print(jsess)
 
-# step2: go to appointment page
+# step2: go to select type of appointment page
 r = session.get("https://rdv.rhone.gouv.fr/eAppointment_PREF69_NAT_DEMANDE/element/jsp/appointment.jsp;jsessionid=" + jsess)
 print(r.text)
 
-# TODO step3: check if 1 available spot
+# TODO step3: select wanted option
 
-# TODO step4: send email
+# TODO step4: go to appointment page
+
+# TODO step5 check if 1 available spot
+
+# TODO step6: send email
