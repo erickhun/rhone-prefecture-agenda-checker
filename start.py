dc54a9468e6360d6183c91c0881ec57b6c1359f6
Eric <enregistrements@gmail.com>
2022-04-08 15:18:07 +0800

    get jsession id and go to appointment page

--- start.py
+++ start.py
@@ -1,11 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib import parse
 
-# step 1: 
+# page 1: load first page
 URL = "https://rdv.rhone.gouv.fr/eAppointment_PREF69_NAT_DEMANDE/appointment.do?preselectservice=DEPOT"
 session = requests.Session()
 session.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'})
 r = session.get(URL)
+print(r.url)
 
+soup = BeautifulSoup(r.text, features="html.parser")
+print(r.url)
+
+# get jsessionid
+jsess = (parse.urlparse(r.url).params).split("=")[1]
+print(jsess)
+
+# step2: go to appointment page
+r = session.get("https://rdv.rhone.gouv.fr/eAppointment_PREF69_NAT_DEMANDE/element/jsp/appointment.jsp;jsessionid=" + jsess)
 print(r.text)
-soup = BeautifulSoup(r.text)
